76ed15a1fc1e999c1dbc8cc3ab69c3742bf78d1c
jeffhowarth <jhowarth@middlebury.edu>
2019-10-19 21:34:04 -0700

    Update eet001.js

--- eet001.js
+++ eet001.js
@@ -1,32 +1,66 @@
 /*
-01 Tutorial 
+001 Tutorial
+
+Mean annual land surface temperature
 
 Jeff Howarth
+10.19.2019
+
+======================================================
+01 Gather and filter data
+======================================================
 */
 
-var lst = ee.ImageCollection('MODIS/006/MOD11A2')
-  .filterDate('2018-01-01', '2018-05-01')
-  .select('LST_Day_1km');
+var lst = // create a container called lst for
+  ee.ImageCollection('MODIS/006/MOD11A2') // an image collection from MODIS 
+  .filterDate('2018-01-01', '2018-05-01') // just keep records between these dates
+  .select('LST_Day_1km'); // just keep this band
 
-print(lst,'LST');
+print(lst,'LST'); //print out this dataset in the console and label it LST
 
-var lstCelsius = lst
-  .mean()
-  .multiply(0.02)
-  .subtract(273.15);
+/*
+=======================================================
+02 Reduce Image Collection to Image and convert units 
+=======================================================
+*/
 
-var palette = [
+var lstCelsius = lst // create a container
+  .mean() // reduce the image collection to the mean values at each location
+  .multiply(0.02) // multiply the value at each location by 0.02 to handle scale factor
+  .subtract(273.15); // subtract by this constant to convert from Kelvin to Celsius
+
+/*
+=======================================================
+03 Define symbology for the image 
+=======================================================
+*/
+
+// the next chunk creates a container to store our palette
+// I took the palette from Color Brewer
+// http://colorbrewer2.org/#type=diverging&scheme=RdYlBu&n=9
+// if you visit this site, you'll notice that the colors go from red to blue
+
+var lstPalette = [
   '#d73027','#f46d43','#fdae61',
   '#fee090','#ffffbf','#e0f3f8',
   '#abd9e9','#74add1','#4575b4'
   ];
 
-var landSurfaceTemperatureVis = {
-  min: 50,
-  max: -50,
-  palette: palette
+// now we'll create a dictionary to store our display ramp
+// the curly brackets {} contain the dictionary
+
+var lstDisplayRamp = {
+  min: 50, // the data value we want to display with the first color in our palette
+  max: -50, // the data value we want to display with the last color in our palette 
+  palette: lstPalette // the list of colors we defined above
 };
 
-Map.setCenter(20,0,2); 
-Map.setOptions('HYBRID');
-Map.addLayer(lstCelsius, landSurfaceTemperatureVis,'Mean Annual Land Surface Temperature',1);
+/*
+=======================================================
+04 Set up map and add layers
+=======================================================
+*/
+
+Map.setCenter(20,0,2); // centers map on 20N longitude, 0 latitude, and zoom factor 2
+Map.setOptions('HYBRID'); // sets the base map to the hybrid option
+Map.addLayer(lstCelsius, lstDisplayRamp,'Mean Annual Land Surface Temperature',1); //adds layer, applies symbology, provides a label
