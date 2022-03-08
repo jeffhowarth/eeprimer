dc1e49fd648860607b444c70fcf9137822ec2b62
jeffhowarth <jhowarth@middlebury.edu>
2022-03-08 14:55:21 -0500

    landsat

---
+++ scripts/landsat.js
@@ -0,0 +1,94 @@
+/*
+    TITLE:  Changes in Aral Sea with Landsat
+    AUTHOR: Jeff Howarth
+    UPDATE: 3/1/2022
+
+    PURPOSE:  In this lesson, we explore the Landsat collections that
+    are available through the Earth Engine Data Catalog. After
+    reviewing a little history and getting acquainted with some of
+    the specialist language/jargon associated with Landsat, we will
+    write a script to visualize changes in the Aral Sea over the last
+    30 years. Working with Surface Reflectance products in Collection
+    Two, we will use a function to scale the data, employ a new method
+    to filter by date, write a custom function to filter different Landsat
+    collections, and examine visible changes in both the landscape and the
+    datasets for studying it.
+*/
+
+
+// Function to apply scaling factors for Landsat Collection 2 Tier 1.
+
+function applyScaleFactors(image) {
+  var opticalBands = image.select('SR_B.').multiply(0.0000275).add(-0.2);
+  var thermalBand = image.select('ST_B.*').multiply(0.00341802).add(149.0);
+  return image.addBands(opticalBands, null, true)
+              .addBands(thermalBand, null, true);
+}
+
+
+var L4 = ee.ImageCollection('LANDSAT/LT04/C02/T1_L2')
+    .filterBounds(geometry.buffer(200 * 1000))
+    .filter(ee.Filter.calendarRange(1989, 1989, 'year'))
+    .filter(ee.Filter.calendarRange(8, 10, 'month'))
+    .filter(ee.Filter.lt('CLOUD_COVER', 30))
+    .map(applyScaleFactors)
+    .mean()
+    ;
+
+var makeLandsatImage = function(ic, year) {
+  var collection = ee.ImageCollection(ic)
+    .filterBounds(geometry.buffer(200 * 1000))
+    .filter(ee.Filter.calendarRange(year, year, 'year'))
+    .filter(ee.Filter.calendarRange(6, 9, 'month'))
+    .filter(ee.Filter.lt('CLOUD_COVER', 20));
+  print(ic + ' ' + String(year), collection.size());
+  return collection
+    .map(applyScaleFactors)
+    .median()
+    ;
+};
+
+var L4_1989 = makeLandsatImage('LANDSAT/LT04/C02/T1_L2', 1989);
+
+var L5_1989 = makeLandsatImage('LANDSAT/LT05/C02/T1_L2', 1989);
+
+var L5_1994 = makeLandsatImage('LANDSAT/LT05/C02/T1_L2', 1994);
+
+var L5_1999 = makeLandsatImage('LANDSAT/LT05/C02/T1_L2', 1999);
+
+var L7_2004 = makeLandsatImage('LANDSAT/LE07/C02/T1_L2', 2004);
+
+var L7_2009 = makeLandsatImage('LANDSAT/LE07/C02/T1_L2', 2009);
+
+var L8_2014 = makeLandsatImage('LANDSAT/LC08/C02/T1_L2', 2014);
+
+var L8_2019 = makeLandsatImage('LANDSAT/LC08/C02/T1_L2', 2019);
+
+print(
+  'L4',
+  // L4.size(),
+  L4);
+
+var viz = {
+  bands: ['SR_B3', 'SR_B2', 'SR_B1'],
+  min: 0.0,
+  max: 0.3,
+};
+
+var viz_L8 = {
+  bands: ['SR_B4', 'SR_B3', 'SR_B2'],
+  min: 0.0,
+  max: 0.3,
+};
+
+Map.centerObject(geometry, 7);
+Map.setOptions('HYBRID');
+
+Map.addLayer(L4_1989, viz, 'L4_1989',0);
+Map.addLayer(L5_1989, viz, 'L5_1989',0);
+Map.addLayer(L5_1994, viz, 'L5_1994',0);
+Map.addLayer(L5_1999, viz, 'L5_1999',0);
+Map.addLayer(L7_2004, viz, 'L7_2004',0);
+Map.addLayer(L7_2009, viz, 'L7_2009',0);
+Map.addLayer(L8_2014, viz_L8, 'L8_2014',0);
+Map.addLayer(L8_2019, viz_L8, 'L8_2019',0);
